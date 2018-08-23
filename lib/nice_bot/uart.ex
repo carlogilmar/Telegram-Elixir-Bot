e8d462403e4c07e234a8fb1c14e328cfeb1d30c6
carlogilmar <carlogilmar12@gmail.com>
2018-08-23 02:49:14 -0500

    Adding genserver for get the current UART process

--- lib/nice_bot/uart.ex
+++ lib/nice_bot/uart.ex
@@ -3,9 +3,23 @@ defmodule NiceBot.Uart do
   use GenServer
 
   # Client
+  def start_link() do
+    GenServer.start_link(__MODULE__, [], [name: __MODULE__])
+  end
 
+  def init_uart( ) do
+    GenServer.call( __MODULE__, :init_uart )
+  end
 
   # Server
+  def init(_) do
+    IO.puts "UART Server init..."
+    {:ok, 1}
+  end
 
+  def handle_call( :init_uart, _, state ) do
+    {:ok, uart} = Nerves.UART.start_link
+    {:reply, uart, state}
+  end
 
 end
